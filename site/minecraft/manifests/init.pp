d527ec221ee063c699b1e88444b48da909864c09
leereyno <leebreynolds@gmail.com>
2022-02-13 11:24:13 -0500

    templates
    Signed-off-by: leereyno <leebreynolds@gmail.com>

--- site/minecraft/manifests/init.pp
+++ site/minecraft/manifests/init.pp
@@ -25,8 +25,13 @@ class minecraft
   }
 
   file {'/etc/systemd/system/minecraft.service':
-    ensure => file,
-    source => 'puppet:///modules/minecraft/minecraft.service',
+    ensure  => file,
+
+    content => epp('minecraft/minecraft.service',
+      {
+        install_dir => $install_dir,
+      }
+    )
   }
 
   service {'minecraft':
